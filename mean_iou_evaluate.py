d8c93416ce60c7720e793f71eca1c8b7a26d53c1
ntudlcv <72860306+ntudlcv@users.noreply.github.com>
2021-10-12 14:54:40 +0800

    Update mean_iou_evaluate.py

--- mean_iou_evaluate.py
+++ mean_iou_evaluate.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.misc
+import imageio
 import argparse
 import os
 
@@ -13,7 +14,7 @@ def read_masks(filepath):
     masks = np.empty((n_masks, 512, 512))
 
     for i, file in enumerate(file_list):
-        mask = scipy.misc.imread(os.path.join(filepath, file))
+        mask = imageio.imread(os.path.join(filepath, file))
         mask = (mask >= 128).astype(int)
         mask = 4 * mask[:, :, 0] + 2 * mask[:, :, 1] + mask[:, :, 2]
         masks[i, mask == 3] = 0  # (Cyan: 011) Urban land 
@@ -22,8 +23,7 @@ def read_masks(filepath):
         masks[i, mask == 2] = 3  # (Green: 010) Forest land 
         masks[i, mask == 1] = 4  # (Blue: 001) Water 
         masks[i, mask == 7] = 5  # (White: 111) Barren land 
-        masks[i, mask == 0] = 6  # (Black: 000) Unknown
-        masks[i, mask == 4] = 6  # (Red: 100) Unknown
+        masks[i, mask == 0] = 6  # (Black: 000) Unknown 
 
     return masks
 
