1f831d971817cf5887daa078159316a5051cde33
ntudlcv <72860306+ntudlcv@users.noreply.github.com>
2021-10-12 14:53:44 +0800

    Update get_dataset.sh

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,9 +1,9 @@
 # Download dataset
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=12RIwW5jN1vrIn8ap1V4-m567I9tUCCBT' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=12RIwW5jN1vrIn8ap1V4-m567I9tUCCBT" -O hw2_data.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1E_X2X-91SVsv0u_b-57wOc_CZuxBKkvJ' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1E_X2X-91SVsv0u_b-57wOc_CZuxBKkvJ" -O hw1_data.zip && rm -rf /tmp/cookies.txt
 
 # Unzip the downloaded zip file
-mkdir hw2_data
-unzip ./hw2_data.zip -d hw2_data
+mkdir hw1_data
+unzip ./hw1_data.zip -d hw1_data
 
 # Remove the downloaded zip file
-rm ./hw2_data.zip
+rm ./hw1_data.zip
